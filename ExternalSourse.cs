a76f50feae2f388c60c8d1714588bd6956cd2107
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-07 01:29:11 +0100

    Add files via upload

---
+++ ExternalSourse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksterni_NameSpace
+{
+     class ExternalSourse
+    {
+        public static void ExternalMetod() 
+        {
+            Console.WriteLine("Pozdrav iz External Metode ");
+        }
+        public static int x = 132;
+    }
+}
+namespace Eksterni_NameSpace1
+{
+    class A
+    {
+        public static void Eksaterni1Print()
+        {
+            Console.WriteLine("Ekstesni 1");
+        }
+    }
+}
+    class B
+    {
+        public static void Eksaterni2Print()
+        {
+            Console.WriteLine("Ekstesni 2");
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Eksterni_NameSpace1;
+
+namespace Eksterni_NameSpace
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ja sam iz glavnog programa");
+            ExternalSourse.ExternalMetod();
+
+            Console.WriteLine(ExternalSourse.x);
+
+            A.Eksaterni1Print();
+            B.Eksaterni2Print();
+        }
+    }
+}
